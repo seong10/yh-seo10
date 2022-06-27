c4f538bef330937bb7f80a2fb8284bf2e6ca7a22
seong10 <kck555511@naver.com>
2022-06-27 17:21:41 +0900

    add

--- my_SQL/6-24_posting.sql
+++ my_SQL/6-24_posting.sql
@@ -36,12 +36,92 @@ delete from posting;
 
 select * from tag_name;
 select * from tag;
+select * from posting;
 
 -- car 로 검색하면,
-select *
+select p.*
 from tag_name tn
 join tag t
 on tn.id = t.tagId
 join posting p
 on p.id = t.postingId
-where tn.name like '%car%';
+where tn.name like '%S%'
+group by t.postingId;
+
+insert into likes
+(postingId, userId)
+values
+(14, 2);
+
+delete from likes
+where postingId = 14 and userId = 2;
+
+select * from likes;
+
+delete from likes;
+
+-- 테이블생성문 만들고싶으면
+-- 테이블 우측마우스 > 카피 투 테이블 > 크리에잇 스테이트먼트
+-- CREATE TABLE `likes` (
+--   `id` int unsigned NOT NULL AUTO_INCREMENT,
+--   `postingId` int DEFAULT NULL,
+--   `userId` int DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 내가 만든 포스팅 수정
+update posting p
+set content = 'asd',
+imgUrl = 'asdf'
+where userId = 2 and p.id = 15 ;
+
+select * from posting;
+
+select p.id
+from posting p
+where userId = 2 and content like '%농%';
+
+-- 태그 삭제
+select * 
+from tag
+where postingId = 15;
+
+delete from tag
+where postingId = 15;
+
+select * 
+from tag t
+join tag_name tn
+on t.tagId = tn.id
+where postingId = 15;
+
+-- 삭제하려면
+-- 삭제할것들은?
+select * from posting;
+-- 포스팅 아이디
+delete from posting
+where id = 15 and userId = 2;
+		-- 이것만 하고 유저아이디가 아니면은 밑에꺼 실행안하면 되는것
+select * from likes;
+-- 포스팅 아이디
+delete from likes
+where postingId = 15;
+
+select * from tag;
+delete from tag
+where postingId = 15;
+
+select * from tag_name;
+
+-- p.*, count(l.postingId), u.email, u.name
+select p.*, u.email, u.name, count(l.postingId) as cnt
+from posting p
+join user u
+on p.userId = u.id
+left join likes l
+on p.id = l.postingId
+where p.userId = 2
+group by p.id;
+
+select *
+from likes;
